331babfcb4cf9550e60113815f41604a84a7e55c
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-06-18 10:49:18 +0600

    Working with test

--- lib/query_report/filter.rb
+++ lib/query_report/filter.rb
@@ -1,7 +1,19 @@
+# Author::    A.K.M. Ashrafuzzaman  (mailto:ashrafuzzaman.g2@gmail.com)
+# License::   MIT-LICENSE
+
+# The purpose of the filter module is to add feature
+# to support report tool to add custom and predefined filters
+
 module QueryReport
   module FilterModule
     attr_accessor :filters
 
+    # Creates a filter and adds to the filters
+    # Params:
+    # +column+:: the column on which the filter is done on
+    # +options+:: Options can have the following,
+    #             options[:type] => date | text | whatever
+    #             options[:comp] => the comparators used for ransack search, [:gteq, :lteq]
     def filter(column, options, &block)
       @filters ||= []
       @filters << Filter.new(@params, column, options, &block)
@@ -10,10 +22,39 @@ module QueryReport
     class Filter
       attr_reader :params, :column, :type, :comparators, :block, :custom
 
+      # Initializes filter with the proper parameters
+      # Params:
+      # +params+:: The params from the http request
+
       def initialize(params, column, options, &block)
         @params = params
         @column = column
         @type = options if options.kind_of? String
+        if options.kind_of? Hash
+          @type = options[:type]
+          @comparators = options[:comp] #|| detect_comparators(@type)
+        end
+        @block = block
+        @custom = @block ? true : false
+      end
+
+      def self.supported_types
+        [:date, :text]
+      end
+
+      def custom?
+        @block ? true : false
+      end
+
+      private
+      def detect_comparators(type)
+        case type
+          when :date
+            return {gteq: I18n.t('query_report.filters.from'), lteq: I18n.t('query_report.filters.to')}
+          when :text
+            return {cont: I18n.t("query_report.filters.#{@column.to_s}")}
+        end
+        {eq: I18n.t('query_report.filters.equal')}
       end
     end
   end
--- spec/report/filter_spec.rb
+++ spec/report/filter_spec.rb
@@ -12,9 +12,30 @@ describe QueryReport::Report do
   end
 
   describe 'filter' do
-    it 'should be able to add filter with supported types' do
-      [:text, :date].each { |type| @object.filter(:created_at, type: type) }
-      @object.filters.size.should be 2
+    it 'should have date and text in the supported list' do
+      QueryReport::FilterModule::Filter.supported_types.should =~ [:date, :text]
     end
+
+    describe 'with supported types' do
+      it 'should be able to filter with type text' do
+        @object.filter(:created_at, type: :text)
+        @object.filters.size.should be 1
+        filter = @object.filters.first
+
+        filter.column.should be :created_at
+        filter.type.should be :text
+        filter.comparators.should =~ [:eq]
+        filter.custom?.should be false
+      end
+    end
+
+    it 'should be able to add custom filter with one arity' do
+      @object.filter(:user_id, type: :user, comp: :text) do |query, user_id|
+        query.where(user_id: user_id)
+      end
+
+      filter = @object.filters.first
+    end
+
   end
 end
