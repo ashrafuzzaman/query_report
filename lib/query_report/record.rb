6a9034f9f1379d0835d226a49985470bed0fe8fd
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-06-22 18:49:09 +0600

    Partially working

---
+++ lib/query_report/record.rb
@@ -0,0 +1,35 @@
+module QueryReport
+  module Record
+    attr_accessor :query
+
+    def model_class
+      query.klass
+    end
+
+    def filtered_query
+      apply
+      @filtered_query
+    end
+
+    def paginated_query
+      apply
+      @paginated_query
+    end
+
+    def apply
+      @filtered_query ||= apply_filters(query.clone, @params)
+      @paginated_query ||= apply_pagination(@filtered_query, @params)
+    end
+
+    def records
+      @records ||= map_record(paginated_query)
+    end
+
+    def map_record(query)
+      query.map do |record|
+        array = @columns.collect { |column| [column.name, column.value(record)] }
+        Hash[*array.flatten]
+      end
+    end
+  end
+end
--- lib/query_report/report.rb
+++ lib/query_report/report.rb
@@ -1,6 +1,7 @@
 require 'query_report/column'
 require 'query_report/filter'
 require 'query_report/paginate'
+require 'query_report/record'
 
 module QueryReport
   DEFAULT_OPTIONS = {chart_on_pdf: true, paginate: true}
@@ -9,9 +10,9 @@ module QueryReport
     include QueryReport::ColumnModule
     include QueryReport::FilterModule
     include QueryReport::PaginateModule
+    include QueryReport::Record
 
     attr_reader :params, :template, :options
-    attr_accessor :query
 
     def initialize(params, template, options={}, &block)
       @params, @template = params, template
@@ -20,38 +21,12 @@ module QueryReport
       instance_eval &block if block_given?
     end
 
-    def model_class
-      query.klass
-    end
-
     # define options methods
     QueryReport::DEFAULT_OPTIONS.each do |option_name, value|
       if value.class == TrueClass or value.class == FalseClass
         define_method "#{option_name.to_s}?" do
           @options[option_name]
         end
-      end
-    end
-
-    def filtered_query
-      apply
-      @filtered_query
-    end
-
-    def apply
-      q = query.clone
-      q = apply_filters(q, @params)
-      @filtered_query = apply_pagination(q, @params)
-    end
-
-    def records
-      @records ||= map_record(filtered_query)
-    end
-
-    def map_record(query)
-      query.map do |record|
-        array = @columns.collect { |column| [column.name, column.value(record)] }
-        Hash[*array.flatten]
       end
     end
   end
