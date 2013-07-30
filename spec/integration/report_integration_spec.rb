f34bf1bd73db6428f18b91189b7adda028165c76
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-07-30 09:23:29 +0600

    Added integration test

--- spec/integration/report_integration_spec.rb
+++ spec/integration/report_integration_spec.rb
@@ -18,56 +18,115 @@ class DummyInvoiceController
 
   def initialize
     @params = {}
+    @view_context = Object.new
+    #@view_context.define_method(:link_to) do |text, *args|
+    #  text
+    #end
   end
 
   def render_report #override the existing renderer
   end
 
-  def index
-    @invoices = DummyInvoice.scoped
-
-    reporter(@invoices) do
-      filter :title, type: :text
-      filter :created_at, type: :date
-
-      column :title do |invoice|
-        #link_to invoice.title, invoice
-        invoice.title
-      end
-      column :total_paid
-      column :total_charged
-      column :paid
-
-      column_chart('Unpaid VS Paid') do
-        add 'Unpaid' do |query|
-          query.sum('total_charged').to_f - query.sum('total_paid').to_f
-        end
-        add 'Paid' do |query|
-          query.sum('total_paid').to_f
-        end
-      end
-    end
-  end
+  #def index
+  #  @invoices = DummyInvoice.scoped
+  #
+  #  reporter(@invoices) do
+  #    filter :title, type: :text
+  #    filter :created_at, type: :date
+  #
+  #    column :title do |invoice|
+  #      #link_to invoice.title, invoice
+  #      invoice.title
+  #    end
+  #    column :total_paid
+  #    column :total_charged
+  #    column :paid
+  #
+  #    column_chart('Unpaid VS Paid') do
+  #      add 'Unpaid' do |query|
+  #        query.sum('total_charged').to_f - query.sum('total_paid').to_f
+  #      end
+  #      add 'Paid' do |query|
+  #        query.sum('total_paid').to_f
+  #      end
+  #    end
+  #  end
+  #end
 end
 
 describe DummyInvoiceController do
-  context "when included by a controller" do
-    it "works" do
-      DummyInvoice.create(title: 'Invoice#1', total_charged: 100, total_paid: 100, paid: true)
-      DummyInvoice.create(title: 'Invoice#2', total_charged: 200, total_paid: 100, paid: false)
-
-      controller = DummyInvoiceController.new
-      controller.index
-      report = controller.instance_eval { @report }
-      ap report.records
-    end
+  before(:each) do
+    DummyInvoice.scoped.destroy_all
+    @inv1 = DummyInvoice.create(title: 'Invoice#1', total_charged: 100, total_paid: 100, paid: true)
+    @inv2 = DummyInvoice.create(title: 'Invoice#2', total_charged: 200, total_paid: 80, paid: false)
+    @inv3 = DummyInvoice.create(title: 'Invoice#3', total_charged: 340, total_paid: 12.5, paid: false)
   end
 
-  #reporter(query, 'ajax_report') do
-  #  filter :name
-  #
-  #  column :name
-  #  column :age
-  #  column :weight
-  #end
+  it "should only show selected columns with readable names" do
+    class DummyInvoiceController
+      def index
+        @invoices = DummyInvoice.scoped
+        reporter(@invoices) do
+          column :title
+          column :total_paid
+        end
+      end
+    end
+
+    controller = DummyInvoiceController.new
+    controller.index
+    report = controller.instance_eval { @report }
+    report.records.should == [{'Title' => @inv1.title, 'Total paid' => @inv1.total_paid},
+                              {'Title' => @inv2.title, 'Total paid' => @inv2.total_paid},
+                              {'Title' => @inv3.title, 'Total paid' => @inv3.total_paid}]
+  end
+
+  it "should initialize with the given query" do
+    class DummyInvoiceController
+      def index
+        @invoices = DummyInvoice.where(paid: false)
+        reporter(@invoices) do
+          column :title
+          column :total_paid
+        end
+      end
+    end
+
+    controller = DummyInvoiceController.new
+    controller.index
+    report = controller.instance_eval { @report }
+    report.records.should == [{'Title' => @inv2.title, 'Total paid' => @inv2.total_paid},
+                              {'Title' => @inv3.title, 'Total paid' => @inv3.total_paid}]
+  end
+
+  context 'filter' do
+    class DummyInvoiceController
+      def index
+        @invoices = DummyInvoice.scoped
+        reporter(@invoices) do
+          filter :paid
+
+          column :title
+          column :total_paid
+        end
+      end
+    end
+
+    #it "should initialize without any filter applied" do
+    #  controller = DummyInvoiceController.new
+    #  controller.index
+    #  report = controller.instance_eval { @report }
+    #  report.records.should == [{'Title' => @inv2.title, 'Total paid' => @inv2.total_paid},
+    #                            {'Title' => @inv3.title, 'Total paid' => @inv3.total_paid}]
+    #end
+    #
+    #it "should initialize without any filter applied" do
+    #  controller = DummyInvoiceController.new
+    #  controller.params[:q] = {paid_eq: '1'}
+    #  controller.index
+    #  report = controller.instance_eval { @report }
+    #  report.records.should == [{'Title' => @inv1.title, 'Total paid' => @inv1.total_paid}]
+    #end
+  end
+
 end
