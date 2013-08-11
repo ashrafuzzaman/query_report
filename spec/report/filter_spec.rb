98a7ae8310927a372d802d9c7ae439551adbd91f
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-08-11 22:14:11 +0600

    refactored filter spec

--- spec/report/filter_spec.rb
+++ spec/report/filter_spec.rb
@@ -23,6 +23,12 @@ describe QueryReport::FilterModule do
       its(:column) { should be :created_at }
       its(:type) { should be :text }
       its(:custom?) { should be false }
+
+      it 'has proper comparators' do
+        comps = subject.comparators
+        comps.collect(&:type).should =~ [:cont]
+        comps.collect(&:name).should =~ [I18n.t('query_report.filters.created_at.contains')]
+      end
     end
 
     context 'with date type' do
@@ -34,6 +40,12 @@ describe QueryReport::FilterModule do
       its(:column) { should be :created_at }
       its(:type) { should be :date }
       its(:custom?) { should be false }
+
+      it 'has proper comparators' do
+        comps = subject.comparators
+        comps.collect(&:type).should =~ [:gteq, :lteq]
+        comps.collect(&:name).should =~ [I18n.t('query_report.filters.from'), I18n.t('query_report.filters.to')]
+      end
     end
 
     context 'with type default' do
@@ -54,58 +66,38 @@ describe QueryReport::FilterModule do
     end
   end
 
-  context 'custom filter' do
-    it 'filters with given block' do
+  context 'with custom filter' do
+    subject do
       object.filter(:user_id, type: :user_auto_complete, comp: {eq: 'Filter user'}) do |query, user_id|
         query.where(user_id: user_id)
       end
-      filter = object.filters.first
+      object.filters.first
+    end
 
-      filter.column.should be :user_id
-      filter.type.should be :user_auto_complete
-      filter.comparators.collect(&:type).should =~ [:eq]
-      filter.comparators.collect(&:name).should =~ ['Filter user']
-      filter.custom?.should be true
+    its(:column) { should be :user_id }
+    its(:type) { should be :user_auto_complete }
+    its(:custom?) { should be true }
+
+    it 'has given comparator' do
+      comps = subject.comparators
+      comps.collect(&:type).should =~ [:eq]
+      comps.collect(&:name).should =~ ['Filter user']
     end
   end
 
-  describe 'detect comparators' do
-    it 'detects for text type' do
-      object.filter(:created_at, type: :text)
-
-      filter = object.filters.first
-      filter.comparators.collect(&:type).should =~ [:cont]
-      filter.comparators.collect(&:name).should =~ [I18n.t('query_report.filters.created_at.contains')]
+  context 'with date type and default values' do
+    subject do
+      object.filter(:created_at, type: :date, default: [1.weeks.ago, Time.zone.now])
+      object.filters.first
     end
 
-    it 'detects for date type' do
-      object.filter(:created_at, type: :date)
+    its(:column) { should be :created_at }
+    its(:type)   { should be :date }
 
-      filter = object.filters.first
-      filter.comparators.collect(&:type).should =~ [:gteq, :lteq]
-      filter.comparators.collect(&:name).should =~ [I18n.t('query_report.filters.from'), I18n.t('query_report.filters.to')]
+    it 'has proper comparator' do
+      comps = subject.comparators
+      comps.collect(&:type).should =~ [:lteq, :gteq]
+      expect(comps.first.has_default?).to be true
     end
-
-    it 'sets default comparator' do
-      object.filter :created_at
-
-      filter = object.filters.first
-      filter.comparators.collect(&:type).should =~ [:eq]
-      filter.comparators.collect(&:name).should =~ [I18n.t("query_report.filters.created_at.equals")]
-    end
-  end
-
-  it 'supports initial filter value' do
-    from = 1.weeks.ago
-    to = Time.now
-    object.filter(:created_at, type: :date, default: [from, to])
-    filter = object.filters.first
-
-    expect(filter.column).to be :created_at
-    expect(filter.type).to be :date
-
-    from_comp = filter.comparators.first
-    expect(from_comp.type).to be :gteq
-    expect(from_comp.has_default?).to be true
   end
 end
