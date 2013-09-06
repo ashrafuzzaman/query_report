2bfba00647d474af46fbddf6b081aafb807949e9
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2013-09-06 22:16:55 +0600

    Only supporting rails 3.X.X

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -18,19 +18,11 @@ ActiveRecord::Schema.define(:version => 20130630132513) do
     t.float    "total_paid"
     t.float    "total_charged"
     t.boolean  "paid"
-    t.integer  "received_by_id"
+    t.string   "received_by"
     t.date     "invoiced_on"
     t.date     "paid_on"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
--- test/dummy/db/seed.rb
+++ test/dummy/db/seed.rb
@@ -1,15 +1,10 @@
 User.scoped.destroy_all
 Invoice.scoped.destroy_all
 
-users = []
-users << User.create(first_name: 'A.K.M.', last_name: 'Ashrafuzzaman', email: 'ashraf@gmail.com')
-users << User.create(first_name: 'A.K.M.', last_name: 'Zahiduzzaman', email: 'zahid@gmail.com')
-users << User.create(first_name: 'Sharmin', last_name: 'Sultana', email: 'sharmin@gmail.com')
-
 1.upto(200) do |i|
   total_paid = 100 + Random.rand(100)
   total_charged = 100 + Random.rand(100)
   invoiced_on = Random.rand(30).days.ago
   Invoice.create!(title: "Invoice ##{i}", total_paid: total_paid, total_charged: total_charged, invoiced_on: invoiced_on,
-                  paid: total_paid >= total_charged, received_by_id: users[Random.rand(3)].id)
+                  paid: total_paid >= total_charged)
 end
