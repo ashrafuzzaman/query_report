e2e73a5ff5bc0b65367dd74a3b43b08bcdd95b35
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-06-01 11:22:23 +0600

    Working with comments

--- lib/query_report/helper.rb
+++ lib/query_report/helper.rb
@@ -11,12 +11,10 @@ module QueryReport
   module Helper
 
     # Generates the reports
-    # Params:
-    # query - The base query that the reporter with start with [filters will be applied on it]
-    # options - Options for the reports
-    #           :per_page - If given then overrides the default kaminari per page option
-    #           :custom_view - by default false, if set to true then the reporter will look for the file to render
-    #           :skip_rendering - by default false, if set to true then the reporter will not render any thing, you will have to implement the rendering
+    # @param query The base query that the reporter with start with [filters will be applied on it]
+    # @option options [Integer] :per_page If given then overrides the default kaminari per page option
+    # @option options [Boolean] :custom_view by default false, if set to true then the reporter will look for the file to render
+    # @option options [Boolean] :skip_rendering by default false, if set to true then the reporter will not render any thing, you will have to implement the rendering
     def reporter(query, options={}, &block)
       @report ||= QueryReport::Report.new(params, view_context, options)
       @report.query = query
