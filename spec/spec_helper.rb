89b4a449a92d4173695e2140b15809f7af886b86
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-06-03 14:26:31 +0600

    Added code climate in travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 begin
   require 'rails'
 rescue LoadError
