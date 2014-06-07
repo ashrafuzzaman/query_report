f964159378e301f931b5f6c50e4663718b47dd34
A.K.M. Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2014-06-07 11:06:53 +0600

    Added show total

--- spec/integration/active_record/column_spec.rb
+++ spec/integration/active_record/column_spec.rb
@@ -4,6 +4,7 @@ require 'integration_helper'
 describe 'column' do
   before do
     @user1 = User.create(name: 'User#1', age: 10, dob: 10.years.ago)
+    @user2 = User.create(name: 'User#2', age: 20, dob: 20.years.ago)
   end
 
   context 'with option :as' do
@@ -12,6 +13,15 @@ describe 'column' do
         column :name, as: 'The name'
       end
     end
-    its(:records) { should == [{'The name' => @user1.name}] }
+    its(:records) { should == [{"The name" => "User#1"}, {"The name" => "User#2"}] }
+  end
+
+  context 'with option :show_total' do
+    subject do
+      reporter(User.scoped) do
+        column :age, show_total: true
+      end
+    end
+    its(:column_total_with_colspan) { should == [{:content => 30.0, :align => :right}] }
   end
 end
